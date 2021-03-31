26b6836c288b38c1c1aa3e77ef2a8543f87a7d71
webyom <webyom@gmail.com>
2021-03-31 16:01:18 +0800

    feat: emulate touch event

--- src/demos/scripts/main.ts
+++ src/demos/scripts/main.ts
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TouchEmulator from 'hammer-touchemulator';
 import { RootComponent } from './root-component';
 import '../../styles/base.scss';
 import '../styles/base.scss';
+
+TouchEmulator();
 
 const container = document.getElementById('app');
 container.innerHTML = '';
