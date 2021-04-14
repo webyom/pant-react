0d8392760324892f99dac87eeeed3851bd12d6be
webyom <webyom@gmail.com>
2021-04-14 11:40:23 +0800

    Updates

--- demos/723.327e2741.js
+++ demos/723.327e2741.js
@@ -1,2 +1,2 @@
 (self.webpackChunkpant_react=self.webpackChunkpant_react||[]).push([[723],{3723:(a,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>n});var c=t(7294);function n(){return c.createElement("img",{className:"lazyload",src:"https://img.yzcdn.cn/vant/apple-1.jpg"})}}}]);
-//# sourceMappingURL=723.2b2f55dd.js.map
+//# sourceMappingURL=723.327e2741.js.map
